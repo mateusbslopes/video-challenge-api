13c098e700f07c11d4af7d11ef98a6dd815f0958
Mateus Lopes <mateusbslopes@gmail.com>
2019-09-14 20:43:21 -0300

    Upload static file to S3.

--- src/main/java/video/challenge/api/Application.java
+++ src/main/java/video/challenge/api/Application.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/video/challenge/api/Service/VideoStorage.java
+++ src/main/java/video/challenge/api/Service/VideoStorage.java
@@ -1,7 +1,15 @@
 package video.challenge.api.Service;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -13,11 +21,24 @@ public class VideoStorage {
     protected static String path = "/home/mateus/workfolder/storage";
 
     public static void save(MultipartFile video){
-        Path filePath = Paths.get(path, video.getOriginalFilename());
+        Regions clientRegion = Regions.SA_EAST_1;
+        String bucketName = "video-challenge-api";
 
-        try (OutputStream os = Files.newOutputStream(filePath)) {
-            os.write(video.getBytes());
-        } catch (IOException e) {
+        String stringObjKeyName = "test1";
+        String fileObjKeyName = "test1";
+        String fileName = "test1";
+
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .build();
+
+            s3Client.putObject(bucketName, stringObjKeyName, "Uploaded String Object");
+
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+        } catch (AmazonServiceException e) {
+            e.printStackTrace();
+        } catch (SdkClientException e) {
             e.printStackTrace();
         }
     }
